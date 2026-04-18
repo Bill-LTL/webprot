53faa986659fc9bd539572a0ec57a74432f73300
Bill <ltl.bill.ltl.fcb@gmail.com>
2026-04-18 17:37:54 +0800

    優化代碼

--- main.c
+++ main.c
@@ -17,6 +17,17 @@ int main() {
     fclose(fp);
     printf("%s api: %s\n", get_now(), cfg.api);
     printf("%s system: %s\n", get_now(), SYSTEM);
+    
+    
+//優先自定義獲取ipv6命令
+    if(strcmp(cfg.cmd_get6, "default") == 0){
+        getipv6 = "ip -6 addr show enp6s0 | grep 'scope global' | grep '/128' | awk '{print $2}' | cut -d'/' -f1";
+        printf("%s 使用預設獲取IPv6命令: %s\n", get_now(), GET_IPV6);  
+            
+    }else{
+        getipv6 = cfg.cmd_get6;
+        printf("%s 使用配置文件獲取IPv6命令: %s\n", get_now(), getipv6);  
+    }
 //--------------------系統檢查     
 
     
