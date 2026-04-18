9f008fd006e9f3f9dca89f13826183834affa3bf
Bill <ltl.bill.ltl.fcb@gmail.com>
2026-04-18 18:03:17 +0800

    優化代碼,讀取文件代碼改爲函數

--- main.c
+++ main.c
@@ -2,40 +2,22 @@
 #include <unistd.h>
 
 int main() {
-//--------------------系統檢查  基本讀取  
-    //讀取配置文件
-    char key[64];
-    char value[64];
-    char line[256];
-    FILE *fp = fopen(".config", "r");
-    while (fgets(line, sizeof(line), fp)) {
-        if(sscanf(line, " %[^ \t=] = %s", key, value) == 2){
-            printf("%s 已解析: key=[%s], value=[%s]\n", get_now(), key, value);
-            auto_fill(key, value);
-        }
-    }
-    fclose(fp);
-    printf("%s api: %s\n", get_now(), cfg.api);
-    printf("%s system: %s\n", get_now(), SYSTEM);
-    
-    
-//優先自定義獲取ipv6命令
+    read_config();    
+    //優先自定義獲取ipv6命令
     if(strcmp(cfg.cmd_get6, "default") == 0){
         getipv6 = "ip -6 addr show enp6s0 | grep 'scope global' | grep '/128' | awk '{print $2}' | cut -d'/' -f1";
-        printf("%s 使用預設獲取IPv6命令: %s\n", get_now(), GET_IPV6);  
+        printf("%s 使用預設獲取IPv6命令: %s\n", get_now(), getipv6);  
             
     }else{
         getipv6 = cfg.cmd_get6;
         printf("%s 使用配置文件獲取IPv6命令: %s\n", get_now(), getipv6);  
     }
-//--------------------系統檢查     
-
-    
+   
     char *ipv6 ;
-    ipv6 = run_cmd(GET_IPV6);
+    ipv6 = run_cmd(getipv6);
 
     while(1){
-        
+        read_config();
         printf("%s %s監控中...\n", get_now(), ipv6);
         sleep(5);
     }
--- somefunc.h
+++ somefunc.h
@@ -3,6 +3,11 @@
 #include <string.h>
 #include <stddef.h>
 #include <stdlib.h>
+const char* get_now();//獲取當前時間函數
+void read_config();   //讀取配置文件函數
+void auto_fill(char *key, char *val);//自動填充配置結構體函數
+char *run_cmd(const char cmd[]);//執行命令並回傳輸出結果函數
+
 
 const char *getipv6;  //於填充函數中獲得賦值「void auto_fill(char *key, char *val)」
 
@@ -11,14 +16,34 @@ const char *getipv6;  //於填充函數中獲得賦值「void auto_fill(char *ke
     #define SYSTEM "windows"
     #define POPEN _popen
     #define PCLOSE _pclose
-    #define  GET_IPV6 "ipconfig"
 #elif defined(__linux__) || defined(__apple__)
     #define SYSTEM "linux"
     #define POPEN popen
     #define PCLOSE pclose
-    #define  GET_IPV6 getipv6
 #endif
 
+/*-------- 配置結構體 ----------*/
+struct Config {
+    char api[64];
+    //char system[64];
+    char cmd_get6[128];
+    char zoneID[64];
+    char recordID[64];
+    char DNS[64];
+} cfg;
+//-----------配置變量偏移量表------------
+struct {
+    char *name;
+    size_t offset;
+} map[] = {
+    {"api",  offsetof(struct Config, api)},
+    //{"system", offsetof(struct Config, system)},
+    {"cmd_get6", offsetof(struct Config, cmd_get6)},
+    {"zoneID", offsetof(struct Config, zoneID)},
+    {"recordID", offsetof(struct Config, recordID)},
+    {"DNS", offsetof(struct Config, DNS)}
+};
+//-------------------------------------
 
 //獲取時間的函數
 const char* get_now() {
@@ -34,26 +59,26 @@ const char* get_now() {
     
     return buffer;
 }
-
+void read_config() {
+//讀取配置文件
+    char key[64];
+    char value[64];
+    char line[256];
+    FILE *fp = fopen(".config", "r");
+    while (fgets(line, sizeof(line), fp)) {
+        if(sscanf(line, " %[^ \t=] = %s", key, value) == 2){
+            printf("%s 已解析: key=[%s], value=[%s]\n", get_now(), key, value);
+            auto_fill(key, value);
+        }
+    }
+    fclose(fp);
+    printf("%s api: %s\n", get_now(), cfg.api);
+    printf("%s system: %s\n", get_now(), SYSTEM);
+}    
 
 //實現填充功能，將key對應到結構體成員並賦值
 
-/*-------- 配置結構體 ----------*/
-struct Config {
-    char api[64];
-    //char system[64];
-    char cmd_get6[128];
-} cfg;
-//-----------配置變量偏移量表------------
-struct {
-    char *name;
-    size_t offset;
-} map[] = {
-    {"api",  offsetof(struct Config, api)},
-    //{"system", offsetof(struct Config, system)},
-    {"cmd_get6", offsetof(struct Config, cmd_get6)}
-};
-//-------------------------------------
+
 
 //匹配key到變量,賦值value,,,,獲取IPv6命令（無則默認）
 void auto_fill(char *key, char *val) {
